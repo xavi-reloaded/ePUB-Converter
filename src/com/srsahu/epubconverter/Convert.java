f2406d73c99e717aaecaebe3b8b9e04a37c8d882
Soumya Ranjan <soumyasahu1986@gmail.com>
2014-02-26 12:37:55 +0530

    license chnages

--- src/com/srsahu/epubconverter/Convert.java
+++ src/com/srsahu/epubconverter/Convert.java
@@ -1,5 +1,5 @@
 /*
-Copyright (C)2013 Soumya Ranjan sahu
+Copyright ©2011 Ezio Querini
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
--- src/com/srsahu/epubconverter/XMLParser.java
+++ src/com/srsahu/epubconverter/XMLParser.java
@@ -1,5 +1,5 @@
 /*
-Copyright (C)2013 Soumya Ranjan sahu
+Copyright (C)2011 Ezio Querini
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
